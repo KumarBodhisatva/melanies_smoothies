75097cd1a5d22888ecaf2c49e525bf1a4dad99bb
KumarBodhisatva <kumar187bodhisatva@gmail.com>
2024-11-03 09:59:59 +0530

    fruitvice api to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session
+import requests
 from  snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
@@ -44,6 +44,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json()) 
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
