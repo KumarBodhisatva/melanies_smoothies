bf00a31c4cabe90f705a8771bd3ec907306f1a85
KumarBodhisatva <kumar187bodhisatva@gmail.com>
2024-11-03 10:02:45 +0530

    fruitvice api moved to for block

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,13 +24,12 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ', '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
     st.write(ingredients_string)
 
@@ -44,6 +43,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
